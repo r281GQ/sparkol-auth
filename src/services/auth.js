395e378fb857279e9cc1e2e21a53b10f04623570
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 22:19:32 +0200

    implement login && logout functions

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,9 +3,37 @@ import React from "react";
 const AuthContext = React.createContext();
 
 const Auth = (props) => {
-  const login = React.useCallback(() => {}, []);
+  const login = React.useCallback(async (data) => {
+    try {
+      // start loading state
 
-  const logout = React.useCallback(() => {}, []);
+      const response = await fetch("http://localhost:3333/login", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (response.status === 401) {
+        throw new Error("Unauthorized");
+      }
+
+      const json = await response.json();
+
+      // set token
+      // update resolved
+
+      return json;
+    } catch (error) {
+      // update rejected
+
+      throw error;
+    }
+  }, []);
+
+  const logout = React.useCallback(() => {
+    // remove user
+    // remove token
+  }, []);
 
   const user = {};
 
