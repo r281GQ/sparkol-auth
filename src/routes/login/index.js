f1ca489362459146a57b59cc2d94935d787de309
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 22:28:43 +0200

    add form to login

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,5 +1,46 @@
+import { css, jsx } from "@emotion/core";
+import { useForm } from "react-hook-form";
+
+/** @jsx jsx */
 const Login = (props) => {
-  return "login";
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+    } catch {}
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div
+        css={css`
+          margin-bottom: 8px;
+        `}
+      >
+        <input
+          name="username"
+          defaultValue="jeff1967"
+          placeholder="username"
+          ref={register}
+        />
+      </div>
+      <div
+        css={css`
+          margin-bottom: 8px;
+        `}
+      >
+        <input
+          name="password"
+          defaultValue="hotdog"
+          placeholder="password"
+          type="password"
+          ref={register}
+        />
+      </div>
+      <input type="submit" value="log in" />
+    </form>
+  );
 };
 
 export default Login;
