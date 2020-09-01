6e3014197b40e2ad7ecb4bf6ed1ff3fc8bc1a6a1
Endre Vegh <endre.vegh@yahoo.com>
2020-09-01 10:00:16 +0200

    add comments to privateroute

--- src/utils/privateRoute.js
+++ src/utils/privateRoute.js
@@ -3,6 +3,12 @@ import { Redirect, Route } from "react-router-dom";
 
 import { useMe } from "../services/auth";
 
+/**
+ * This is a reusable component to prevent access to routes
+ * which should be protected unless authorized.
+ *
+ * It just composes two react-router-dom elements based if we have an auth user.
+ */
 const PrivateRoute = (props) => {
   const user = useMe();
 
