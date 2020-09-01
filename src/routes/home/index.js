7fd4397922aa975daaafe0554059d0771e2c3cfe
Endre Vegh <endre.vegh@yahoo.com>
2020-09-01 09:55:32 +0200

    add comments  to home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,17 +1,34 @@
-import { css, jsx } from "@emotion/core";
+import { jsx } from "@emotion/core";
 
 import { useMe, useLogout } from "../../services/auth";
 
+import Header from "../../components/header";
+import Layout from "../../components/layout";
+import Main from "../../components/main";
+
+/**
+ * Home is protected but the logic is not tied to <Home />.
+ *
+ * Other components might be private in the future.
+ *
+ * See <PrivateRoute /> for implementation.
+ *
+ * It uses the useMe hook for getting the user and the useLogout hook to log out.
+ */
 /** @jsx jsx */
 const Home = () => {
   const user = useMe();
   const [logout] = useLogout();
 
   return (
-    <div>
-      <button onClick={logout}>log out</button>
-      <div>{`Welcome ${user.name}!`}</div>
-    </div>
+    <Layout>
+      <Header>
+        <div>
+          <button onClick={logout}>log out</button>
+        </div>
+      </Header>
+      <Main>{`Welcome ${user.name}!`}</Main>
+    </Layout>
   );
 };
 
