b70382110687c17fa3f9eb13832938bb12c594f8
Endre Vegh <endre.vegh@yahoo.com>
2020-09-01 10:00:08 +0200

    add comments to token

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,5 +1,16 @@
 let t;
 
+/**
+ * Shared mutable state. I rarely use this but this seems a good use-case.
+ *
+ * Why?
+ *
+ * No UI state depends on it. Auth state is manages by the <Auth /> service.
+ *
+ * This data is purely for providing the access token for API requests.
+ *
+ * It need to bypass potential closures and saves states.
+ */
 export const setToken = (token) => {
   t = token;
 };
