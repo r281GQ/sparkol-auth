97cba0ddeb90a988cf47909d9656ed0742fabe9d
Endre Vegh <endre.vegh@yahoo.com>
2020-09-01 09:24:35 +0200

    use privateroute for home

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,18 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./home";
 import Login from "./login";
 
+import PrivateRoute from "../utils/privateRoute";
+
+/**
+ * I believe "react-router-dom" is much smarter than me
+ * and takes care of preventing unwanted re-renders inside "<BrowserRouter />".
+ *
+ * But since it is the direct child of Context Auth, React.memo can come handy.
+ */
 const Routes = React.memo(function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
+      <PrivateRoute path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
     </BrowserRouter>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
 export * from "./token";
+export { default as PrivateRoute } from "./privateRoute";
